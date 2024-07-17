ccc7f6bdc2daff8d0ecdce48f5fc710ecf95fda3
Mike030790 <98897271+Mike030790@users.noreply.github.com>
2024-07-17 12:26:00 -0600

    Create index.php

---
+++ Parcial 2/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reservaciones de Hotel</title>
+</head>
+<body>
+    <h1>Reservaciones de Hotel</h1>
+    <?php include_once 'formulario_reserva.php'; ?>
+</body>
+</html>
